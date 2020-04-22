5179878fef6ff2a0ca031382b5fa098499d23a92
chinmairam <chinmairammuppa@gmail.com>
2020-04-22 11:04:16 +0530

    Create Category resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_182717) do
+ActiveRecord::Schema.define(version: 2020_04_22_050754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menu_items", force: :cascade do |t|
     t.integer "menu_id"
