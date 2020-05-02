998c6283eaedc09d1e608677a95fbf1955e1a1e2
chinmairam <chinmairammuppa@gmail.com>
2020-05-02 20:44:41 +0530

    Added MenuItems Methods

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,27 +1,38 @@
 class MenuItemsController < ApllicationController
-  skip_before_action :verify_authenticity_token
-  skip_before_action :ensure_user_logged_in
-
   def index
-    @menu_id = params[:menu_id]
-    @menu_items = MenuItem.return(@menu_id)
-    @menu_name = Menu.find(@menu_id).menu_name
+  end
+
+  def show
   end
 
   def create
-    MenuItem.create!(
-      name: params[:name],
-      description: params[:description],
-      price: params[:price],
-      menu_id: params[:menu_id],
-    )
-    redirect_to menu_items_path(menu_id: params[:menu_id])
+    menu = Menu.where(name: params[:menu_name].capitalize).exists? ? Menu.where(name: params[:menu_name].capitalize).first : Menu.new(name: params[:menu_name].capitalize)
+    menu.save
+    menu_item = MenuItem.new(name: params[:name].capitalize, description: params[:description].capitalize, menu_id: menu.id, price: params[:price])
+    if menu.save && menu_item.save
+      redirect_to menus_path
+    else
+      redirect_to menus_path
+    end
   end
 
   def destroy
-    id = params[:id]
-    menu_item = MenuItem.find(id)
-    menu_item.destroy
-    redirect_to menu_items_path(menu_id: params[:menu_id])
+    ensure_owner_logged_in
+    MenuItem.find(params[:id]).destroy
+    redirect_to menus_path
+  end
+
+  def edit
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def update
+    menu = Menu.where(name: params[:menu_name].capitalize).exists? ? Menu.where(name: params[:menu_name].capitalize).first : Menu.new(name: params[:menu_name].capitalize)
+    menu.save
+    menu_item = MenuItem.find(params[:id])
+    menu_item.update(name: params[:name].capitalize, description: params[:description].capitalize, menu_id: menu.id, price: params[:price])
+    if menu.save && menu_item.save
+      redirect_to menus_path
+    end
   end
 end
