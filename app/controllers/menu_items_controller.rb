1c50b4dd85bbe49b66c0259cff88f86c48742c24
chinmairam <chinmairammuppa@gmail.com>
2020-05-02 21:24:59 +0530

    Added MenuItems List

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,28 +2,29 @@ class MenuItemsController < ApllicationController
   def index
   end
 
-  def show
-  end
-
   def create
     menu = Menu.where(name: params[:menu_name].capitalize).exists? ? Menu.where(name: params[:menu_name].capitalize).first : Menu.new(name: params[:menu_name].capitalize)
     menu.save
     menu_item = MenuItem.new(name: params[:name].capitalize, description: params[:description].capitalize, menu_id: menu.id, price: params[:price])
     if menu.save && menu_item.save
       redirect_to menus_path
+      flash[:notice] = "Item added successfully!"
     else
       redirect_to menus_path
     end
+  end
+
+  def show
+  end
+
+  def edit
+    @menu_item = MenuItem.find(params[:id])
   end
 
   def destroy
     ensure_owner_logged_in
     MenuItem.find(params[:id]).destroy
     redirect_to menus_path
-  end
-
-  def edit
-    @menu_item = MenuItem.find(params[:id])
   end
 
   def update
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -16,6 +16,7 @@ class MenusController < ApplicationController
   end
 
   def destroy
+  ensure_owner_logged_in
     id = params[:id]
     menu = Menu.find(id)
     menu_items = MenuItem.allwhere("menu_id=?", id)
