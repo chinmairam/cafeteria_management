957e83446b7aa3b41280f0bbb765a216fa3c708a
chinmairam <chinmairammuppa@gmail.com>
2020-05-02 20:53:19 +0530

    Added Menu views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,37 @@
 class ApplicationController < ActionController::Base
-    before_action :ensure_user_logged_in
-  
-    def ensure_user_logged_in
-      
-  
-    def current_user
-      return @current_user if @current_user
-  
-      current_user_id = session[:current_user_id]
-      if current_user_id
-        @current_user = User.find(current_user_id)
-      else
-        nil
-      end
+  before_action :ensure_user_logged_in
+
+  def ensure_user_logged_in
+    unless current_user
     end
+  end
+
+  def ensure_owner_logged_in
+    unless current_user.is_owner?
+      redirect_to menus_path
+    end
+  end
+
+  def ensure_clerk_logged_in
+    unless current_user.is_clerk?
+      redirect_to menus_path
+    end
+  end
+
+  def ensure_owner_or_clerk_logged_in
+    unless current_user.is_clerk? || current_user.is_owner?
+      redirect_to menus_path
+    end
+  end
+
+  def current_user
+    return @current_user if @current_user
+
+    current_user_id = session[:current_user_id]
+    if current_user_id
+      @current_user = User.find(current_user_id)
+    else
+      nil
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
-    
+  has_secure_password
+
+  def is_clerk?
+    clerk = role == "clerk" ? true : false
+  end
+
+  def is_owner?
+    owner = role == "owner" ? true : false
+  end
+
+  def self.clerks
+    where("role = ?", "clerk")
+  end
+
+  def self.customers
+    where("role = ?", "customer")
+  end
+
+  def self.get_user_by_email(email)
+    user = all.where("email = ?", email).exists? ? find_by(email: email) : false
+  end
 end
