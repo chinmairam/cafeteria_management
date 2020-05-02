38944711cae9fead93ebf17d853d0c2ff17c1e4d
chinmairam <chinmairammuppa@gmail.com>
2020-05-02 21:59:56 +0530

    Modified Home Page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,14 @@
 class OrdersItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :menu_item
+
+  def self.get_menu_item_price(menu_item_name)
+    find_by(menu_item_name: menu_item_name).menu_item_price
+  end
+
+
+  def all_menu_item_names
+    order_items.order(:menu_item_name).map { |item| item.menu_item_name }
+  end
+  
 end
